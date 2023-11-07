bf81cad50d44f7232bfb16ff429d8dce72eb7a28
Ukudot <giovannipanico314@gmail.com>
2023-11-07 14:23:38 +0100

    mariadb works

--- mariaDB/tools/script.sh
+++ mariaDB/tools/script.sh
@@ -2,11 +2,11 @@
 service mariadb start
 
 # mariadb-secure-installation
-mariadb -e "CREATE DATABASE $MARIADB_DATABASE"
-mariadb -e "CREATE USER '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_USER_PASSWORD'"
-mariadb -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'localhost'"
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD'"
-mariadb -uroot -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES"
+mariadb -e "CREATE DATABASE $MARIADB_DATABASE;"
+mariadb -e "CREATE USER '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
+mariadb -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
+mariadb -uroot -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
 
 # stop mariadb
 mysqladmin -uroot -p$MARIADB_ROOT_PASSWORD shutdown
