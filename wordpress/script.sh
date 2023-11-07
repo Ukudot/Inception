7f440dcf273e3364e068156c185d7db5f053df79
Ukudot <giovannipanico314@gmail.com>
2023-11-07 16:26:15 +0100

    working on nginx

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -11,8 +11,8 @@ apt install php php-curl php-fpm php-bcmath php-gd php-soap php-zip php-curl php
 service php8.2-fpm start
 
 # download wordpress
-wget https://wordpress.org/latest.zip
-unzip latest.zip -d /var/www/html/
+wget 'https://it.wordpress.org/wordpress-6.3.2.zip'
+unzip 'wordpress-6.3.2.zip' -d /var/www/html/
 cd /var/www/html/wordpress
 cp wp-config-sample.php wp-config.php
 sed -i "s/database_name_here/wordpress_db/" wp-config.php
