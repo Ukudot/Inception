0d76665bf8afeba62d1b5caa7e8593734145265d
Ukudot <giovannipanico314@gmail.com>
2023-11-09 15:56:31 +0100

    wordpress in progress

--- wordpress/script.sh
+++ wordpress/script.sh
@@ -12,14 +12,14 @@ service php8.2-fpm start
 
 # download wordpress
 wget 'https://it.wordpress.org/wordpress-6.4.1-it_IT.zip'
-unzip 'wordpress-6.4.1-it_IT.zip' -d /var/www
-cd /var/www/wordpress
+unzip 'wordpress-6.4.1-it_IT.zip' -d /var/www/html
+cd /var/www/html/wordpress
 cp wp-config-sample.php wp-config.php
 sed -i "s/nome_del_database_qui/inception_db/" wp-config.php
 sed -i "s/nome_utente_qui/username/" wp-config.php
 sed -i "s/password_qui/password/" wp-config.php
 sed -i "s/'DB_HOST', 'localhost'/'DB_HOST', 'mariadb:3306'/" wp-config.php
-chown -R www-data:www-data /var/www/wordpress/
+chown -R www-data:www-data /var/www/html/wordpress/
 wget https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
-mv wp-cli.phar /usr/local/bin/wp
+mv wp-cli.phar /usr/local/bin/wp-cli.phar
--- wordpress/tools/config.sh
+++ wordpress/tools/config.sh
@@ -1,17 +1,21 @@
-wp config create --allow-root \
+wp-cli.phar config create --allow-root \
 	--dbname=$SQL_DATABASE \
 	--dbuser=$SQL_USER \
 	--dbpass=$SQL_PASSWORD \
-	--dbhost=mariadb:3306 --path='/var/www/wordpress'
+	--dbhost=mariadb:3306 \
+	--path='/var/www/wordpress'
 
-wp core install --url=$DOMAIN_NAME \
+wp-cli.phar core install --allow-root \
+	--url=$DOMAIN_NAME \
 	--title=$WP_TITLE \
 	--admin_user=$WP_ADMIN \
 	--admin_password=$WP_ADMIN_PASSWORD \
 	--admin_email=$WP_ADMIN_EMAIL \
-	--skip-email
+	--skip-email \
+	--path='/var/www/wordpress'
 
-wp user create $WP_USER $WP_USER_EMAIL \
+wp-cli.phar user create $WP_USER $WP_USER_EMAIL --allow-root \
 	--role='author' \
 	--user_pass=$WP_USER_PASSWORD \
-	--porcelain
+	--porcelain \
+	--path='/var/www/wordpress'
