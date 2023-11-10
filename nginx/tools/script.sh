f4142d7d7af0268436b71f2e1b107870fc173ce8
Ukudot <giovannipanico314@gmail.com>
2023-11-10 09:55:33 +0100

    wordpress in progress

--- nginx/tools/script.sh
+++ nginx/tools/script.sh
@@ -2,7 +2,7 @@
 mkdir -p /etc/nginx/$CERTS_
 openssl req -x509 -noenc -out /etc/nginx/$CERTS_/inception.crt \
 	-keyout /etc/nginx/$CERTS_/inception.key \
-	-subj "/C=IT/ST=Lazio/L=Roma/O=42/OU=trinity/CN={$DOMAIN_NAME}/UID=gpanico" > /dev/null
+	-subj "/C=IT/ST=Lazio/L=Roma/O=42/OU=trinity/CN=$DOMAIN_NAME/UID=gpanico" 2> /dev/null
 envsubst '$CERTS_ $DOMAIN_NAME' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
 rm /etc/nginx/conf.d/nginx.conf.template
 exec nginx -g "daemon off;"
