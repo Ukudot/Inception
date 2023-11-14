ff699412b00d2ca1b8eaf86ca7a097a182743499
Ukudot <giovannipanico71@yahoo.it>
2023-11-14 08:16:12 +0100

    docker compose done

--- mariaDB/tools/script.sh
+++ mariaDB/tools/script.sh
@@ -2,17 +2,21 @@
 # start mariadb service
 service mariadb start
 
-# mariadb-secure-installation
-mariadb -uroot -e "CREATE DATABASE $MARIADB_DATABASE;"
-sleep .1
-mariadb -uroot -e "CREATE USER '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
-sleep .1
-mariadb -uroot -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
-sleep .1
-mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
-sleep .1
-mariadb -uroot -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
-sleep .1
+# check if this script has been already launched
+
+mariadb -uroot -e "SHOW DATABASES;" 1> /dev/null 2>&1 && (
+	# mariadb-secure-installation
+	mariadb -uroot -e "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE;"
+	sleep .1
+	mariadb -uroot -e "CREATE USER IF NOT EXISTS '$MARIADB_USER'@'localhost' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
+	sleep .1
+	mariadb -uroot -e "GRANT ALL PRIVILEGES ON $MARIADB_DATABASE.* TO '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_USER_PASSWORD';"
+	sleep .1
+	mariadb -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MARIADB_ROOT_PASSWORD';"
+	sleep .1
+	mariadb -uroot -p$MARIADB_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
+	sleep .1
+)
 
 # stop mariadb
 mysqladmin -uroot -p$MARIADB_ROOT_PASSWORD shutdown
--- nginx/tools/script.sh
+++ nginx/tools/script.sh
@@ -3,6 +3,9 @@ mkdir -p /etc/nginx/$CERTS_
 openssl req -x509 -noenc -out /etc/nginx/$CERTS_/inception.crt \
 	-keyout /etc/nginx/$CERTS_/inception.key \
 	-subj "/C=IT/ST=Lazio/L=Roma/O=42/OU=trinity/CN=$DOMAIN_NAME/UID=gpanico" 2> /dev/null
-envsubst '$CERTS_ $DOMAIN_NAME' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
-rm /etc/nginx/conf.d/nginx.conf.template
+if test -f /etc/nginx/conf.d/nginx.conf.template
+then
+	envsubst '$CERTS_ $DOMAIN_NAME' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/nginx.conf
+	rm /etc/nginx/conf.d/nginx.conf.template
+fi
 exec nginx -g "daemon off;"
